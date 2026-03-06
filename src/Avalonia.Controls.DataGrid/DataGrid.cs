7fd1e70038a1f643ac97f87b9c4b52e39e842344
Julien Lebosquain <julien@lebosquain.net>
2026-03-06 11:33:52 +0100

    Update Avalonia to 12.0.0-preview2

--- src/Avalonia.Controls.DataGrid/DataGrid.cs
+++ src/Avalonia.Controls.DataGrid/DataGrid.cs
@@ -3989,42 +3989,14 @@ namespace Avalonia.Controls
             _focusedObject = null;
             if (ContainsFocus)
             {
-                bool focusLeftDataGrid = true;
-                bool dataGridWillReceiveRoutedEvent = true;
-                var focusedObject = TopLevel.GetTopLevel(this)?.FocusManager?.GetFocusedElement() as Visual;
+                var focusedObject = TopLevel.GetTopLevel(this)?.FocusManager.GetFocusedElement() as Visual;
+                var focusLeftDataGrid = !this.IsVisualAncestorOf(focusedObject);
+
                 DataGridColumn editingColumn = null;
-
-                while (focusedObject != null)
-                {
-                    if (focusedObject == this)
-                    {
-                        focusLeftDataGrid = false;
-                        break;
-                    }
-
-                    // Walk up the visual tree.  If we hit the root, try using the framework element's
-                    // parent.  We do this because Popups behave differently with respect to the visual tree,
-                    // and it could have a parent even if the VisualTreeHelper doesn't find it.
-                    var parent = focusedObject.Parent as Visual;
-                    if (parent == null)
-                    {
-                        parent = focusedObject.GetVisualParent();
-                    }
-                    else
-                    {
-                        dataGridWillReceiveRoutedEvent = false;
-                    }
-                    focusedObject = parent;
-                }
 
                 if (EditingRow != null && EditingColumnIndex != -1)
                 {
                     editingColumn = ColumnsItemsInternal[EditingColumnIndex];
-
-                    if (focusLeftDataGrid && editingColumn is DataGridTemplateColumn)
-                    {
-                        dataGridWillReceiveRoutedEvent = false;
-                    }
                 }
 
                 if (focusLeftDataGrid && !(editingColumn is DataGridTemplateColumn))
@@ -4044,7 +4016,7 @@ namespace Avalonia.Controls
                         }
                     }
                 }
-                else if (!dataGridWillReceiveRoutedEvent)
+                else if (focusLeftDataGrid)
                 {
                     if (focusedObject is Control focusedElement)
                     {
